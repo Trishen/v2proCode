b57611480503d2d6e23d77188ac30efa1caaa160
Trishen <trishendragon@yahoo.com>
2020-07-28 15:57:42 +0200

    create

---
+++ v2proCode/v2proCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(v2proCode.Startup))]
+namespace v2proCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
